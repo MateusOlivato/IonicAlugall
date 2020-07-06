7a5a5972227c337dc78236eb1eb4e02e09f296c6
Serinolli <rafael.serinolli2@gmail.com>
2020-07-06 15:50:50 -0300

    Formulários
    Primeiro passo para implementação de formulários e validações concluído.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,6 +10,34 @@ import { Component } from '@angular/core';
 })
 export class LoginPage{
 
-  constructor() { }
+  public formLogin: FormGroup;
+
+  public mensagens_validacao = {
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail inválido' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório digitar a senha' },
+      { tipo: 'minlenght', mensagem: 'A senha deve ter pelo menos 6 caracteres.' },
+      { tipo: 'maxlenght', mensagem: 'A senha deve ter no máximo 10 caracteres.' }
+    ],
+    confirma: [],
+
+  };
+
+  constructor(public formBuilder: FormBuilder) {this.formLogin = formBuilder.group({
+
+      //Aqui, declara-se todos os campos do formulário
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      senha: ['', Validators.compose([Validators.minLength(6), Validators.maxLength(20), Validators.required])]
+
+    });
+  }
+
+  public login(){
+    
+  }
 
 }
+
